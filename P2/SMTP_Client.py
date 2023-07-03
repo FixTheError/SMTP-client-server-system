69fc611790b68afc0016a1de6fd740173531af58
FixTheError <89999029+FixTheError@users.noreply.github.com>
2023-07-03 14:13:59 -0700

    Update SMTP_Client.py
    Added some comments to client driver script.

--- P2/SMTP_Client.py
+++ P2/SMTP_Client.py
@@ -3,7 +3,10 @@ import sys
 import re
 import codecs
 import Client_classes
+
+#Check for the config files.
 if (len(sys.argv) == 3):
+    #Open the config files and read them into memory, assigning server information to variables..
     sender_config = open(sys.argv[1], "r")
     receiver_config = open(sys.argv[2], "r")
     tmp = sender_config.readline()
@@ -17,12 +20,16 @@ if (len(sys.argv) == 3):
     tmp_list = tmp.split("=")
     HTTP_Port = tmp_list[1]
 else:
+    #Config files were not included, this should probably return.
     print("Error, expected: SMTP_Client sender.conf reciever.conf")
+
+#Parse the host IP
 ip_regex = re.findall(r'(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})', HOST)
 
-
+#Set up a TCP socket.
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     done = False
+    
     while(done != True):
         print ("For sender SMTP connection type SMTP, for reciever HTTP connection type HTTP\n")
         conn_type = input()
