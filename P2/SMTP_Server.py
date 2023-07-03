05772c4a66c57704678b0df1d37c0fada9174343
FixTheError <89999029+FixTheError@users.noreply.github.com>
2023-07-02 19:58:26 -0700

    Update SMTP_Server.py
    Added comments to the main server file.

--- P2/SMTP_Server.py
+++ P2/SMTP_Server.py
@@ -5,10 +5,11 @@ import Server_classes
 import threading
 
 
-
+#Make sure the config file was included as a parameter.
 if (len(sys.argv) < 2):
     print ("config file not included")
 
+#Create and start threads for handling SMTP and HTTP clients.
 SMTP_Thread = threading.Thread(target = Server_classes.SMTP_Handler, args = (sys.argv[1],))
 HTTP_Thread = threading.Thread(target = Server_classes.HTTP_Handler, args = (sys.argv[1],))
 SMTP_Thread.start()
